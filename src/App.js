5ab4546bf72ede4ab139ecce11894e2309b331e5
kaancembertas <kaancembertas@gmail.com>
2019-11-09 17:25:25 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+  function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="myCanvas" width="800" height="500"
+      style={{marginTop:'10px',border:'1px solid #c3c3c3'}}>
+
+      </canvas>
     </div>
   );
 }
